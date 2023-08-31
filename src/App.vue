59ec0ce477359b9801ee2ae0f0ce167583102775
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-08-31 17:29:35 +0200

    Added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import SnakeGame from "./components/games/snake/SnakeGame.vue";
-import SnakeHome from "./components/games/snake/SnakeHome.vue";
+
 </script>
 
 <template>
-  <SnakeHome/>
-<!--  <SnakeGame/>-->
+  <router-view/>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
+import SnakeHome from "./components/games/snake/SnakeHome.vue";
+import SnakeGame from "./components/games/snake/SnakeGame.vue";
 
-createApp(App).mount('#app')
+const routes: RouteRecordRaw[] = [
+    { path: "/", component: SnakeHome },
+    { path: "/game", component: SnakeGame },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
+
+const createdApp = createApp(App);
+
+createdApp.use(router);
+createdApp.mount('#app');
