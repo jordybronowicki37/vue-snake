36ede0fa078d3e32b10574cab0b8cd7b8bce346c
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-02 15:50:17 +0200

    Added smooth route transition

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-  <router-view/>
+  <router-view v-slot="{Component}">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="$route.path"></component>
+    </transition>
+  </router-view>
 </template>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
