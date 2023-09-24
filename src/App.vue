f731dc003c4414276887bece90759350df9ea4db
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-09-24 15:17:09 +0200

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-
+import Header from "./components/header/Header.vue";
 </script>
 
 <template>
+  <Header/>
   <router-view v-slot="{Component}">
     <transition name="fade" mode="out-in">
       <component :is="Component" :key="$route.path"></component>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header">
+    <h1>Snake Game</h1>
+    <nav class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/menu">Game-Modes</router-link>
+      <a href="https://github.com/jordybronowicki37/vue-snake" target="_blank">Github</a>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  background-color: #383838;
+  padding: 1rem;
+}
+.header h1 {
+  margin: 0;
+}
+.links {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-grow: 1;
+  gap: 1rem;
+}
+.links>* {
+  font-weight: 700;
+  font-size: 20px;
+  color: #eee;
+}
+.links>*:hover {
+  color: #999;
+}
+.links .router-link-active {
+  color: var(--difficulty-2);
+}
+</style>
