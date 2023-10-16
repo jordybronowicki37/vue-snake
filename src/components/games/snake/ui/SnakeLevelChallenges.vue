d045885f27c6924633efe8a115faed3094ce2b89
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-10-16 22:33:10 +0200

    Added achieved challenge animation

--- src/components/games/snake/ui/SnakeLevelChallenges.vue
+++ src/components/games/snake/ui/SnakeLevelChallenges.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import {SnakeLevelChallengePreview} from "../engine/SnakeTypes.ts";
+import {ref} from "vue";
 
-defineProps<{ challenges: SnakeLevelChallengePreview[] }>()
+const {challenges} = defineProps<{ challenges: SnakeLevelChallengePreview[] }>();
+const oldState = ref(challenges.map(v => v.completed));
 </script>
 
 <template>
   <div class="level-challenges">
     <h3>Level challenges</h3>
     <div>
-      <div class="level-challenge" v-for="challenge in challenges">
-        <div class="star-indicator" :class="[challenge.completed?'star-3':'star-0']"/>
+      <div class="level-challenge" v-for="(challenge, index) in challenges" :key="index">
+        <div
+            class="star-indicator"
+            :class="[
+                challenge.completed ? 'star-3' : 'star-0',
+                !oldState[index] && challenge.completed ? 'new-star' : ''
+            ]"/>
         <p>{{challenge.title}}</p>
       </div>
     </div>
@@ -34,5 +41,29 @@ defineProps<{ challenges: SnakeLevelChallengePreview[] }>()
   height: 15px;
   background-repeat: no-repeat;
   background-size: cover;
+}
+.new-star {
+  animation: tada 1s;
+}
+@keyframes tada {
+  0% {
+    transform: scale(1);
+  }
+
+  10%, 20% {
+    transform: scale(.8) rotate(-3deg);
+  }
+
+  30%, 50%, 70%, 90% {
+    transform: scale(1.3) rotate(3deg);
+  }
+
+  40%, 60%, 80% {
+    transform: scale(1.3) rotate(-3deg);
+  }
+
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
